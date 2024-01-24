bd023fe321f7a503cde028866281e062954eba85
Tigran Bayburtsyan <t@tigran.tech>
2024-01-24 22:16:41 +0400

    adding files

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# face-detection-tests
+# ABOUT
+
+This project is a test implementation of extracting unique faces detected on YouTube videos with a given URL. I have not implemented CLI tools or video ID extraction. It is a plain coded URL that you must change or extend functionality to deliver new video URLs or process batch videos.
+
+The core of this project is built on top of the `face_recognition` and `rembg` Python libraries, which are doing pretty much all of the work; I just made a composable reading from a given video file using Python OpenCV.
+
+## Running Locally
+
+This project doesn’t have external service dependencies, and everything is inside an `[experiment.py](http://experiment.py)` file, where it will handle the video download using the `pytube` library and write extracted frames inside the same directory.
+
+```bash
+pip install -r requirements.txt
+
+python experiment.py
+```
+
+## What’s Next Here?
+
+This project doesn’t have that much in it, because it was very small functionality that I am building for one of my products within `[treescale.com](http://treescale.com)` as a Face detection API Endpoint.
---
+++ experiment.py
@@ -0,0 +1,61 @@
+import cv2
+from pytube import YouTube
+from rembg import remove
+import face_recognition
+
+# (YouTube('https://www.youtube.com/watch?v=kl8Pd-k1yxE').streams
+#  .filter(file_extension='mp4')
+#  .order_by('resolution').desc()
+#  .first()
+#  .download(output_path=".", filename="video.mp4"))
+#
+# exit(0)
+
+vidcap = cv2.VideoCapture('video.mp4')
+
+
+def face_encoding(img):
+    face_enc = face_recognition.face_encodings(img)
+    if len(face_enc) == 0:
+        return None
+    return face_enc[0]
+
+
+if __name__ == "__main__":
+    faces = []
+    frame_index = 0
+    while True:
+        success, image = vidcap.read()
+        frame_index += 1
+        if not success:
+            break
+        if frame_index % 30 != 0:
+            continue
+
+        face_encodings = face_encoding(image)
+        if face_encodings is not None:
+            found_same = False
+            for face in faces:
+                is_same = face_recognition.compare_faces([face['encoding']], face_encodings)[0]
+                if is_same:
+                    face['counter'] += 1
+                    found_same = True
+                    break
+            if not found_same:
+                faces.append({
+                    "frame": image,
+                    "encoding": face_encodings,
+                    "counter": 1
+                })
+                print("New face found as frame", frame_index)
+
+    print("Total faces found", len(faces))
+    print("Saving PNG Frames...")
+    count = 0
+    for face in faces:
+        if face['counter'] > 10:
+            face_frame = remove(face['frame'])
+            cv2.imwrite("frame%d.png" % count, face_frame)
+            count += 1
+    print("Done!")
+
